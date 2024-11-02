7bbce72c5421883ea38821f064b87558a07566e2
FastyXx <90849099+FastyXx@users.noreply.github.com>
2024-11-02 23:43:43 +0100

    Update start.sh
    added monitor script.

--- start.sh
+++ start.sh
@@ -1,2 +1,20 @@
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
+#!/bin/bash
+
+# Installation directory
+INSTALL_DIR="/sdcard/ccminer"
+
+# Name of the ccminer process
+PROCESS_NAME="ccminer"
+
+# Start ccminer
+~/ccminer/ccminer -c ~/ccminer/config.json &
+
+# Monitor ccminer and restart if it stops
+while true; do
+  ps aux | grep "$PROCESS_NAME" > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "ccminer is not running, restarting..."
+    ~/ccminer/ccminer -c ~/ccminer/config.json &
+  fi
+  sleep 60
+done
